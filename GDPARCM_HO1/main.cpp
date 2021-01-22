75ef348f3c30b2c8c5ce7a71e22f06844df9ffc3
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2021-01-22 10:37:52 +0800

    Revised ho1 to make it simpler.

--- GDPARCM_HO1/main.cpp
+++ GDPARCM_HO1/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-#include "BaseRunner.h"
+#include "HelloWorldThread.h"
+#include "IETThread.h"
+
+void createHWThreads()
+{
+	for (int i = 0; i < 20; i++)
+	{
+		HelloWorldThread* thread = new HelloWorldThread(i);
+		thread->start();
+	}
+	
+}
 int main() {
-	BaseRunner runner;
-	runner.run();
+	createHWThreads();
+
+	//Sleep main thread to avoid premature halting. Wait for other threads to finish execution.
+	IETThread::sleep(1000); 
 }
